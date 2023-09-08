4d84ebee20a6fb00cfb3b7056e997ba69fb85cdb
Yonas3s <119069245+Yonas3s@users.noreply.github.com>
2023-09-08 09:20:54 +0300

    Add files via upload

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -7,7 +7,7 @@ module.exports = (req, res, next) => {
   const { authorization } = req.headers;
 
   if (!authorization || !authorization.startsWith('Bearer ')) {
-    throw new UnauthorizedStatus('Необходима авторизация.');
+    return new UnauthorizedStatus('Необходима авторизация.');
   }
 
   const token = authorization.replace('Bearer ', '');
@@ -16,7 +16,7 @@ module.exports = (req, res, next) => {
   try {
     payload = jwt.verify(token, SECRET_KEY);
   } catch (err) {
-    throw new UnauthorizedStatus('Необходима авторизация.');
+    return new UnauthorizedStatus('Необходима авторизация.');
   }
   req.user = payload;
   next();
